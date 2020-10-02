1830282173f3776c255135da277097f37ef1a773
nandang <nandangsopyan08@gmail.com>
2020-10-02 15:15:41 +0700

    remove unused import

--- MyIntentApp/app/src/main/java/com/example/myintentapp/MoveActivity.kt
+++ MyIntentApp/app/src/main/java/com/example/myintentapp/MoveActivity.kt
@@ -1,10 +1,7 @@
 package com.example.myintentapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 
 class MoveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
