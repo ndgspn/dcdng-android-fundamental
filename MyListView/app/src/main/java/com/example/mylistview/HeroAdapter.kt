70000d371965700c61a9542381478f1a18e54cc8
nandang <nandangsopyan08@gmail.com>
2020-10-14 11:46:13 +0700

    ListView

---
+++ MyListView/app/src/main/java/com/example/mylistview/HeroAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.mylistview
+
+import android.content.Context
+import android.media.Image
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import java.security.AccessControlContext
+
+class HeroAdapter internal constructor(private val context: Context) : BaseAdapter() {
+    internal var heroes = arrayListOf<Hero>()
+    override fun getCount(): Int {
+        return heroes.size
+    }
+
+    override fun getItem(i: Int): Any {
+        return heroes[i]
+    }
+
+    override fun getItemId(i: Int): Long {
+        return i.toLong()
+    }
+
+    override fun getView(position: Int, view: View?, viewGroup: ViewGroup?): View {
+        var itemView = view
+        if (itemView == null) {
+            itemView = LayoutInflater.from(context).inflate(R.layout.item_hero, viewGroup, false)
+        }
+
+        val viewHolder = ViewHolder(itemView as View)
+
+        val hero = getItem(position) as Hero
+        viewHolder.bind(hero)
+        return itemView
+    }
+
+    private inner class ViewHolder internal constructor(view: View) {
+        private val tvName: TextView = view.findViewById(R.id.tv_name)
+        private val tvDescription: TextView = view.findViewById(R.id.tv_description)
+        private val imgPhone: ImageView = view.findViewById(R.id.img_photo)
+
+        internal fun bind(hero: Hero) {
+            tvName.text = hero.name
+            tvDescription.text = hero.description
+            imgPhone.setImageResource(hero.photo)
+        }
+    }
+}
