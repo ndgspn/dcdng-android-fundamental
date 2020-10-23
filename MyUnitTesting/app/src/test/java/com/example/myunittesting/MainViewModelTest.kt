804c4bf93655667e1274d238829abb621e1df950
Nandang <nandangsopyan08@gmail.com>
2020-10-23 13:17:47 +0700

    MyUnitTesting - Mock

--- MyUnitTesting/app/src/test/java/com/example/myunittesting/MainViewModelTest.kt
+++ MyUnitTesting/app/src/test/java/com/example/myunittesting/MainViewModelTest.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 
 class MainViewModelTest {
 
@@ -52,6 +52,31 @@ class MainViewModelTest {
         assertEquals(dummyVolume, volume, 0.0001)
 
     }
+
+    @Test
+    fun testMockVolume() {
+        `when` (mainViewModel.getVolume()).thenReturn(dummyVolume)
+        val volume = mainViewModel.getVolume()
+        verify(cuboidModel).getVolume()
+        assertEquals(dummyVolume, volume, 0.0001)
+    }
+
+    @Test
+    fun testMockCircumference() {
+        `when`(mainViewModel.getCirumference()).thenReturn(dummyCircumference)
+        val volume = mainViewModel.getCirumference()
+        verify(cuboidModel).getCircumference()
+        assertEquals(dummyCircumference, volume, 0.0001)
+    }
+
+    @Test
+    fun testMockSurfaceArea() {
+        `when`(mainViewModel.getSurfaceArea()).thenReturn(dummySurfaceArea)
+        val volume = mainViewModel.getSurfaceArea()
+        verify(cuboidModel).getSurfaceArea()
+        assertEquals(dummySurfaceArea, volume, 0.0001)
+    }
+
 
     @Test
     fun save() {
